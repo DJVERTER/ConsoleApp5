280fb6d0e89ae7ef65190ad86fd8507e1dc94d15
DJVERTER <creativekira1982@gmail.com>
2022-01-14 11:53:55 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<string> str = new List<string> { "true", "||", "false" };
+            string result = str[2];
+            bool myBool;
+            
+                if (bool.TryParse(result, out myBool))
+                {
+                    Console.WriteLine(myBool);
+                }
+                else if(result == str[1])
+                {
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine(myBool);
+                }
+
+        }
+    }
+}
