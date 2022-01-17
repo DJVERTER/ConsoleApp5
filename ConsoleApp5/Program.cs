f4c83918a36ba6b9a4de676ce558060d81aea8c9
DJVERTER <creativekira1982@gmail.com>
2022-01-17 11:03:31 +0200

    add enum

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -3,28 +3,90 @@ using System.Collections.Generic;
 
 namespace ConsoleApp6
 {
+    abstract class Transformer
+    {
+        protected int wheels;
+        protected int speed;
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        public int Wheels
+        {
+            get { return wheels; }
+            set { wheels = value; }
+            
+        }
+        public abstract void Run();
+    }
+
+    class Jet : Transformer
+    {
+        public override void Run()
+        {
+            Wheels = 3;
+            Speed = 800;
+            Console.WriteLine($"Transformer is transformed in Jet, haw {wheels} wheels and fly from sped: {speed}");
+        }
+    }
+
+    class Car : Transformer
+    {
+        public override void Run()
+        {
+            Wheels = 4;
+            Speed = 220;
+            Console.WriteLine($"Transformer is transform in Car, haw {wheels} wheels and sped is: {speed}");
+        }
+    }
+
+    class Ship : Transformer
+    {
+        public override void Run()
+        {
+            Speed = 40;
+            Console.WriteLine($"Transformer is transform in Ship, are swimm whit sped: {speed}");
+        }
+    }
     class Program
     {
-        
+        enum LandScape
+        {
+            Transformer,
+            Air,
+            Road,
+            Water
+        }
         static void Main(string[] args)
         {
-            List<string> str = new List<string> { "true", "||", "false" };
-            string result = str[2];
-            bool myBool;
+            Transformer jet = new Jet();
+            Transformer car = new Car();
+            Transformer ship = new Ship();
             
-                if (bool.TryParse(result, out myBool))
-                {
-                    Console.WriteLine(myBool);
-                }
-                else if(result == str[1])
-                {
-                    Console.WriteLine(result);
-                }
-                else
-                {
-                    Console.WriteLine(myBool);
-                }
+            LandScape now = LandScape.Water;
+            Transformation(now);
 
+            void Transformation(LandScape land)
+            {
+                switch (land)
+                {
+                    case LandScape.Transformer:
+                        Console.WriteLine("I,m a Transformer");
+                        break;
+                    case LandScape.Air:
+                        jet.Run();
+                        break;
+                    case LandScape.Road:
+                        car.Run();
+                        break;
+                    case LandScape.Water:
+                        ship.Run();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
